86f4d0a721734dffa65655d9a89acc9d88209b78
Oleksiy Perkin <perkin.oleksiy@gmail.com>
2020-10-04 12:58:37 +0300

    generated GithubsController, added an endpoint inside api/vi namespaces to routes.rb file

---
+++ app/controllers/api/v1/githubs_controller.rb
@@ -0,0 +1,8 @@
+module Api
+  module V1
+    class GithubsController < ApplicationController
+      def index
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
-# frozen_string_literal: true
-
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do 
+    namespace :v1 do 
+      root to: 'githubs#index'
+    end
+  end   
 end
