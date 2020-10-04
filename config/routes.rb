2fdd7a99e4b22f32df869ecd2af94ef67db200ae
Oleksiy Perkin <perkin.oleksiy@gmail.com>
2020-10-04 19:38:23 +0300

    Formated all code by eslint and prettier gems. Changed default title, deleted react icon from tab

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  namespace :api do 
-    namespace :v1 do 
+  namespace :api do
+    namespace :v1 do
       root to: 'githubs#index'
     end
-  end   
+  end
 end
--- spec/requests/api/v1/githubs_request_spec.rb
+++ spec/requests/api/v1/githubs_request_spec.rb
@@ -1,41 +1,41 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "Githubs", type: :request do
-
-  describe "GET /api/v1" do
-    it "given empty string, return Not Found" do 
+RSpec.describe 'Githubs', type: :request do
+  describe 'GET /api/v1' do
+    it 'given empty string, return Not Found' do
       VCR.use_cassette('api/v1') do
-        get "/api/v1"
+        get '/api/v1'
         expect(json['message']).to eq('Not Found')
         expect(response).to have_http_status(404)
       end
     end
 
-    it "given search parameter with asdasd value, return No name" do 
+    it 'given search parameter with asdasd value, return No name' do
       VCR.use_cassette('api/v1?search=asdasd') do
-        get "/api/v1?search=asdasd"
+        get '/api/v1?search=asdasd'
         expect(json['data']['name']).to eq('No name')
         expect(json['data']['repos']).to eq([])
         expect(response).to have_http_status(200)
       end
     end
 
-    it "given search parameter with o-perkin value, return name and repos" do 
+    it 'given search parameter with o-perkin value, return name and repos' do
       VCR.use_cassette('api/v1?search=o-perkin') do
-        get "/api/v1?search=o-perkin"
+        get '/api/v1?search=o-perkin'
         expect(json['data']['name']).to eq('Oleksii Perkin')
         expect(json['data']['repos']).not_to eq([])
         expect(response).to have_http_status(200)
       end
     end
 
-    it "given search parameter with no github user value, return Not Found" do 
+    it 'given search parameter with no github user value, return Not Found' do
       VCR.use_cassette('api/v1?search=lkjasdljkasd') do
-        get "/api/v1?search=lkjasdljkasd"
+        get '/api/v1?search=lkjasdljkasd'
         expect(json['message']).to eq('Not Found')
         expect(response).to have_http_status(404)
       end
     end
   end
-
 end
