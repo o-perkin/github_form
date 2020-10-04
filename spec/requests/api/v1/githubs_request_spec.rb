168479a796d859094ad88a8d7fd29aecf1113145
Oleksiy Perkin <perkin.oleksiy@gmail.com>
2020-10-04 18:52:59 +0300

    Added request spec with tests for api/v1 endpoint

--- spec/requests/api/v1/githubs_request_spec.rb
+++ spec/requests/api/v1/githubs_request_spec.rb
@@ -2,12 +2,38 @@ require 'rails_helper'
 
 RSpec.describe "Githubs", type: :request do
 
-  describe "GET api/v1" do
-    it "given a username, it returns the correct user object" do 
+  describe "GET /api/v1" do
+    it "given empty string, return Not Found" do 
       VCR.use_cassette('api/v1') do
-        get "api/v1"
-        expect(user.class).to eq(Twitter::User)
-        expect(user.username).to eq("sm_debenedetto") 
+        get "/api/v1"
+        expect(json['message']).to eq('Not Found')
+        expect(response).to have_http_status(404)
+      end
+    end
+
+    it "given search parameter with asdasd value, return No name" do 
+      VCR.use_cassette('api/v1?search=asdasd') do
+        get "/api/v1?search=asdasd"
+        expect(json['data']['name']).to eq('No name')
+        expect(json['data']['repos']).to eq([])
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    it "given search parameter with o-perkin value, return name and repos" do 
+      VCR.use_cassette('api/v1?search=o-perkin') do
+        get "/api/v1?search=o-perkin"
+        expect(json['data']['name']).to eq('Oleksii Perkin')
+        expect(json['data']['repos']).not_to eq([])
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    it "given search parameter with no github user value, return Not Found" do 
+      VCR.use_cassette('api/v1?search=lkjasdljkasd') do
+        get "/api/v1?search=lkjasdljkasd"
+        expect(json['message']).to eq('Not Found')
+        expect(response).to have_http_status(404)
       end
     end
   end
