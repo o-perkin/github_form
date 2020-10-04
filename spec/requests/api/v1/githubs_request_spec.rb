dcd8e9c52e57129e884c74b344635e741fa24080
Oleksiy Perkin <perkin.oleksiy@gmail.com>
2020-10-04 18:38:14 +0300

    Created helper method for parsing json response

--- spec/requests/api/v1/githubs_request_spec.rb
+++ spec/requests/api/v1/githubs_request_spec.rb
@@ -2,10 +2,13 @@ require 'rails_helper'
 
 RSpec.describe "Githubs", type: :request do
 
-  describe "GET /index" do
-    it "returns http success" do
-      get "/githubs/index"
-      expect(response).to have_http_status(:success)
+  describe "GET api/v1" do
+    it "given a username, it returns the correct user object" do 
+      VCR.use_cassette('api/v1') do
+        get "api/v1"
+        expect(user.class).to eq(Twitter::User)
+        expect(user.username).to eq("sm_debenedetto") 
+      end
     end
   end
 
